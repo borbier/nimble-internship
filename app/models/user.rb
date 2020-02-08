d0bb983661538efdcb487b0999fbd4ec2edb0d5d
Yuttakhan Baingen <yuttakhanbaingen@gmail.com>
2020-02-08 21:28:34 +0700

    :rewind: Revert username to email

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,18 +3,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  validates :username, uniqueness: true
-
-  def email_required?
-    false
-  end
-
-  def email_changed?
-    false
-  end
-
-  def will_save_change_to_email?
-    false
-  end
 end
