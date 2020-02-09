71155a23f498e450d3e2d9e7995a320634d8f166
Yuttakhan Baingen <yuttakhanbaingen@gmail.com>
2020-02-09 19:49:59 +0700

    :sparkles: Override registration controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
   # def new
@@ -10,9 +10,36 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    # super
+    render plain: params[:user].inspect
+    firstName = params[:user][:firstName]
+    lastName = params[:user][:lastName]
+    password = params[:user][:password]
+    email = params[:user][:email]
+    citizenId = params[:user][:citizenId]
+    role = params[:user][:role]
+
+    @new_user = User.create(firstName: firstName, lastName: lastName, password: password, email: email, citizenId: citizenId, role: role)
+
+    res_owner_id = @new_user.id
+
+    case @new_user.role
+      when "student"
+        puts "student!"
+
+        @studentId = RoleResource.new(name: "StudentId", resType: "string", resValue: params[:regStudentId])
+        @new_user.roleResources << @studentId
+      when "teacher"
+        puts "teacher!"
+        @teacherId = RoleResource.create(name: "TeacherId", resType: "string", resValue: params[:regTeacherId])
+        @new_user.roleResources << @teacherId
+        @department = RoleResource.create(name: "Department", resType: "string", resValue: params[:regDepartmentId])
+        @new_user.roleResources << @department
+      else
+        puts "Nay!"
+    end
+  end
 
   # GET /resource/edit
   # def edit
@@ -41,19 +68,19 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
+  end
 
   # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def after_sign_up_path_for(resource)
+    '/'
+  end
 
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
