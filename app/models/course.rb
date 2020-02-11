29fe9306bf0685c4ae59c608c8da32e54c66d80a
Yuttakhan Baingen <yuttakhanbaingen@gmail.com>
2020-02-11 20:58:47 +0700

    :sparkles: Add model

---
+++ app/models/course.rb
@@ -0,0 +1,4 @@
+class Course < ApplicationRecord
+  has_many :course_user
+  has_many :users, :through => :course_user
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ApplicationRecord
 
   enum role: [:teacher, :student, :outsider]
   has_many :roleResources, dependent: :destroy
+
+  has_many :course_user
+  has_many :courses, :through => :course_user
 end
