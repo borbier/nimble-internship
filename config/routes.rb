e3b2977e138c44876da1ab0db0acd9c405bdcb50
Yuttakhan Baingen <yuttakhanbaingen@gmail.com>
2020-02-11 23:30:04 +0700

    :wrench: Update route configuration

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     :registrations => 'users/registrations'
   }
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  authenticated :user do
+    resources :courses
+  end
+
   root to: 'home#index'
 end
