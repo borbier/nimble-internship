d919d969c9eebb774ee9a369eb234a9d378263f5
Yuttakhan Baingen <yuttakhanbaingen@gmail.com>
2020-02-12 13:55:47 +0700

    :white_check_mark: Add test

---
+++ test/controllers/group_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class GroupControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get group_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get group_create_url
+    assert_response :success
+  end
+
+end
