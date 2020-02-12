7af0d7ded8d0972c2c4cc88643d2029fe8a2510d
Yuttakhan Baingen <yuttakhanbaingen@gmail.com>
2020-02-12 22:42:08 +0700

    :card_file_box: Add migration/update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_11_165536) do
+ActiveRecord::Schema.define(version: 2020_02_12_151236) do
 
   create_table "course_users", force: :cascade do |t|
     t.integer "course_id"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 2020_02_11_165536) do
 
   create_table "courses", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "group_users", force: :cascade do |t|
+    t.integer "group_id"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
