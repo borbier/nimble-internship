5c81baa8b3ecfd4fca3f1b5458b71759c01e207f
Yuttakhan Baingen <yuttakhanbaingen@gmail.com>
2020-02-12 14:18:38 +0700

    :sparkles: Add controllers for add group to course

--- app/controllers/group_controller.rb
+++ app/controllers/group_controller.rb
@@ -1,7 +1,14 @@
 class GroupController < ApplicationController
   def new
+    @group = Group.new
   end
 
   def create
+    @group = Group.create group_params
+    group.courses << @course
+  end
+
+  private def group_params
+    params.require(:group).permit(:name)
   end
 end
